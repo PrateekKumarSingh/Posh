39004e1ece850726a52598038d062d985a08985d
Prateek Singh <Prateeksingh1590@gmail.com>
2017-01-13 16:24:28 +0530

    Update Get-Sentence.ps1

--- WebScraping/Get-Sentence.ps1
+++ WebScraping/Get-Sentence.ps1
@@ -10,27 +10,37 @@ Function Get-Sentence
 
     Try
     {
-        $Results = Invoke-WebRequest "http://sentence.yourdictionary.com/$Word"
+        $Results = Invoke-WebRequest "http://sentence.yourdictionary.com/$Word" -TimeoutSec 5 -DisableKeepAlive
     }
     catch
     {
-        Write-host $_ -ForegroundColor Red
+        Write-host $_ -ForegroundColor Green
     }
     
     $i=0
-    If($Results.StatusCode -eq 200)
+    If(-not $Results)# -and $Results.StatusCode -eq 200)
+    {
+            Write-Host "Couldn't find any sentences for word : $Word" -ForegroundColor Red
+    }
+    else
     {
         Foreach($Sentence in ($Results.ParsedHtml.getElementsByTagName('Div')| Where{$_.ClassName -eq 'li_content'}|Get-Random -Count $count))
         {
             $i=$i+1
-            ''|Select @{n='#';e={$i}},
-                      @{n='WordCount';e={$Sentence.textContent.Split(' ').count}},
-                      @{n='Sentence';e={$Sentence.textContent}}
+            $WordCount = $Sentence.textContent.Split(' ').count
+            If($MaxWords -and $WordCount -le $MaxWords)
+            {
+                ''|Select @{n='#';e={$i}},
+                          @{n='WordCount';e={$WordCount}},
+                          @{n='Sentence';e={$Sentence.textContent}}
+            }
+            elseif(-not $MaxWords)
+            {
+                ''|Select @{n='#';e={$i}},
+                          @{n='WordCount';e={$WordCount}},
+                          @{n='Sentence';e={$Sentence.textContent}}
+            }
         }
-    }
-    else
-    {
-        Write-Host "Couldn't find any sentences for word : $Word" -ForegroundColor Red
     }
    
 }
