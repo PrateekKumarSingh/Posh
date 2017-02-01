67e35754afd99446dd0488f9f5c3b7a65bdbaa44
Prateek Singh <Prateeksingh1590@gmail.com>
2017-02-01 23:08:16 +0530

    Update Trace-Word.ps1

--- Cosmetics/Trace-Word.ps1
+++ Cosmetics/Trace-Word.ps1
@@ -31,7 +31,7 @@ Function Trace-Word
 
         For($i=0;$i -lt $words.count ;$i++)
         {
-            if($i -gt 13)
+            if($i -eq 13)
             {
                 $j =0
             }
@@ -70,42 +70,3 @@ Function Trace-Word
 }
 
 #gc .\log.txt | Trace-Word -words "random","more","on",'of','end','logging','is','a','test','log','some','information','this','file','and','goes'
-
-Function Trace-Word
-{
-    [Cmdletbinding()]
-    [Alias("Highlight")]
-    Param(
-            [Parameter(ValueFromPipeline=$true)] [string[]] $content,
-            [String[]]$words = $(throw "Provide word[s] to be highlighted!"),
-            [Switch] $MatchCase
-    )
-    
-    Begin
-    {
-        if($MatchCase){$Operator = '-cin'}
-        else{$Operator = '-in' }
-    }
-    Process
-    {
-
-    $content | ForEach-Object {
-                    
-        $_.split() | `
-        where{-not [string]::IsNullOrWhiteSpace($_)} | ` #Filter-out whiteSpaces
-        ForEach-Object{       
-            If(Invoke-Expression "$_ $operator $(($words|%{"`'$_`'"}) -join ',')"){
-                Write-Host "$_" -NoNewline -Fore Black -Back Yellow;
-                Write-Host " " -NoNewline
-            }
-            else{
-                Write-Host "$_ " -NoNewline
-            }
-        }
-        Write-Host '' #New Line               
-    }
-    }
-    end
-    {    }
-
-}
