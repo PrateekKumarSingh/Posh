391e2d73ab4a4c37c03e848890ce188a4460d63b
Prateek Singh <Prateeksingh1590@gmail.com>
2017-05-10 21:02:09 +0530

    Create AliasTest.ps1

---
+++ Cosmetics/AliasTest.ps1
@@ -0,0 +1,12 @@
+[System.Management.Automation.PSParser]::Tokenize((gc $psISE.CurrentPowerShellTab.Files.fullpath), [ref]$null) | ?{$_.type -eq 'command'} |`
+ %{
+    $token = $_.content
+
+    $ResolvedCommand = (Get-Alias |?{$_.Name -eq $token}).resolvedcommand.Name
+    if(-not [string]::IsNullOrEmpty($ResolvedCommand))
+    {
+        "$Token - $ResolvedCommand"
+    }
+
+    gci;gci;ls;ls;ls;wmio
+ }
