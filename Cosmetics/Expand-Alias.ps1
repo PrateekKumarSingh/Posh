6125f29f4230dc1bbb0228b181171df088fcfdf1
Prateek Singh <Prateeksingh1590@gmail.com>
2017-05-10 20:02:53 +0530

    Update Expand-Alias.ps1

--- Cosmetics/Expand-Alias.ps1
+++ Cosmetics/Expand-Alias.ps1
@@ -1,47 +1,63 @@
 Function Expand-Alias
 {
+    [CmdletBinding()]
     Param( 
-            [Parameter(Mandatory=$true)]
+            [Parameter(
+                Mandatory=$true,
+                ValueFromPipeline=$true,
+                ValueFromPipelineByPropertyName=$true,
+                Position=0
+                )
+            ]
             [ValidateScript({Test-Path $_ -PathType 'leaf'})]  
-            [string] $Path  
+            [string] $Path,
+            [Switch] $UpdateFile
     )
-
 
     Begin{}
     Process{
-                $Reader = [system.io.file]::OpenText($Path)
-                
-                $RequiredFixes = Invoke-ScriptAnalyzer -Path $Path | `
-                Where {$_.RuleName -eq "PSAvoidUsingCmdletAliases"} | `
-                Select-Object @{n='Target';e={$_.extent}}, `
-                @{n='Correction';e={$_.SuggestedCorrections.text}}, `
-                @{n='StartIndex';e={$_.SuggestedCorrections.StartColumnNumber -1 }}, `
-                @{n='EndIndex';e={$_.SuggestedCorrections.EndColumnNumber - 1}}, `
-                @{n='StartLineNumber';e={$_.SuggestedCorrections.StartLinenumber}}, `
-                @{n='EndLineNumber';e={$_.SuggestedCorrections.EndlineNumber}}
-                
-                $i = 1
-                While($true)
+                ForEach($P in $Path)
                 {
-                    #$i
-                    $Line = $Reader.ReadLine()
-                    If($i -in $RequiredFixes.StartLineNumber)
-                    {
-                        $Item = $RequiredFixes|Where-Object {$_.StartLinenumber -eq $i}
+                    $Reader = [system.io.file]::OpenText($P)
 
-                        $Line -replace "$($Line.substring($Item.StartIndex,$Item.Target.Text.Length))" , $Item.Correction
-                    }
-                    else
+                    $RequiredFixes = Invoke-ScriptAnalyzer -Path $Path | `
+                    Where-Object {$_.RuleName -eq "PSAvoidUsingCmdletAliases"} | `
+                    Select-Object @{n='Target';e={$_.extent}}, `
+                    @{n='Correction';e={$_.SuggestedCorrections.text}}, `
+                    @{n='StartIndex';e={$_.SuggestedCorrections.StartColumnNumber -1 }}, `
+                    @{n='EndIndex';e={$_.SuggestedCorrections.EndColumnNumber - 1}}, `
+                    @{n='StartLineNumber';e={$_.SuggestedCorrections.StartLinenumber}}, `
+                    @{n='EndLineNumber';e={$_.SuggestedCorrections.EndlineNumber}}
+
+                    $i = 1
+                    $Result = @()
+                    While($Reader.Peek() -gt 0)
                     {
-                        $Line    
+                        #$i
+                        $Line = $Reader.ReadLine()
+                        If($i -in $RequiredFixes.StartLineNumber)
+                        {
+                            $Item = $RequiredFixes|Where-Object {$_.StartLinenumber -eq $i}
+                            #To escape Replace the special char in
+                            $ToReplace = [Regex]::Escape("$($Line.substring($Item.StartIndex,$Item.Target.Text.Length))")
+                            $Result =  $Result + $($Line -replace $ToReplace , $Item.Correction)
+                        }
+                        else
+                        {
+                            $Result =  $Result + $Line    
+                        }
+                        $i=$i+1
                     }
-                    $i=$i+1
+                    $Reader.Close()
+                    If($UpdateFile)
+                    {
+                        $Result | Out-File "$(Convert-path $P)" -Force -Verbose
+                    }
+                    $Result
+
                 }
-
-                $Reader.Close()
+                    #$Path | Trace-Word -words $RequiredFixes.target.text
     }
     End{}
 
 }
-
-# ls | ForEach-Object {Invoke-ScriptAnalyzer -Path $_.FullName} |group Rulename |sort -Descending count
